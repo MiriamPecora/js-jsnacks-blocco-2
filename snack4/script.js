62e207adb0ddcf568dba15f7d3347a0918e70cd0
MiriamPecora <miriampecora97@gmail.com>
2024-05-07 10:16:48 +0200

    first commit

---
+++ snack4/script.js
@@ -0,0 +1,14 @@
+// Crea due array che hanno un numero di elementi diversi.
+
+// Aggiungi elementi casuali all’array che ha meno elementi,
+
+// fino a quando ne avrà tanti quanti l’altro.
+
+const shorterArray = [1, 2, 3, 4, 5];
+const longerArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+while(shorterArray.length < longerArray.length){
+    let addition = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+    shorterArray.push(addition)
+    console.log(shorterArray)
+}
